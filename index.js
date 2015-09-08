318b2c544a4e4edc3cf47aa1e4cb789c1aae56b5
Hans Kristian Flaatten <hans.kristian.flaatten@turistforeningen.no>
2015-09-08 22:43:16 +0200

    Add initial NRK TV app API endpoints

--- index.js
+++ index.js
@@ -4,18 +4,76 @@ const jsonist = require('jsonist');
 
 const nrk_tv_api   = process.env.NRK_TV_API   || 'https://tv.nrk.no';
 const nrk_ps_api   = process.env.NRK_PS_API   || 'http://v8.psapi.nrk.no';
+const nrk_tv_mobil = process.env.NRK_TV_MOBIL;
 
 module.exports._opts = function _opts(agent) {
-  return {
-    headers: {
-      'user-agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 6_0 like Mac OS X) AppleWebKit/536.26 (KHTML, like Gecko) Version/6.0 Mobile/10A5376e Safari/8536.25',
-      'accept': 'application/json, text/javascript, */*; q=0.01',
-      'x-requested-with': 'XMLHttpRequest',
-    }
-  };
+  if (agent === 'app') {
+    return {
+      headers: {
+        'User-Agent': 'NRK%20TV/43 CFNetwork/711.5.6 Darwin/14.0.0',
+        'accept': '*/*',
+        'app-version-ios': '43',
+        'Accept-Language:': 'en-us',
+      }
+    };
+  } else {
+    return {
+      headers: {
+        'user-agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 6_0 like Mac OS X) AppleWebKit/536.26 (KHTML, like Gecko) Version/6.0 Mobile/10A5376e Safari/8536.25',
+        'accept': 'application/json, text/javascript, */*; q=0.01',
+        'x-requested-with': 'XMLHttpRequest',
+      }
+    };
+  }
 };
 
 module.exports.tv = {};
+module.exports.tv.mobil = {};
+
+/**
+ * Get TV categories
+ *
+ * @param cb - callback function (err, data, res)
+ *
+ * @return Array of categories
+ */
+module.exports.tv.mobil.categories = function categories(cb) {
+  const url = `${nrk_tv_mobil}/categories`;
+  jsonist.get(url, module.exports._opts('app'), cb);
+};
+
+/**
+ * Get one ore more TV program
+ *
+ * @param pid - program id
+ * @param cb - callback function (err, data, res)
+ *
+ * @return Array of programs
+ */
+module.exports.tv.mobil.programs = function programs(pid, cb) {
+  let url;
+
+  if (!pid) {
+    url = `${nrk_tv_mobil}/programs`;
+  } else {
+    url = `${nrk_tv_mobil}/programs/${pid}`;
+  }
+
+  jsonist.get(url, module.exports._opts('app'), cb);
+};
+
+/**
+ * Search for TV programs
+ *
+ * @param str - string to search for
+ * @param cb - callback function (err, data, res)
+ *
+ * @return Array of matching programs
+ */
+module.exports.tv.mobil.search = function search(str, cb) {
+  const url = `${nrk_tv_mobil}/search/${encodeURIComponent(str)}`;
+  jsonist.get(url, module.exports._opts('app'), cb);
+};
 
 /**
  * Get programs matching string (autocomplete)
---
+++ test/tv.mobil.js
@@ -0,0 +1,207 @@
+'use strict';
+
+const assert = require('assert');
+const Joi = require('joi');
+const nrk = require('../');
+
+describe('categories()', function describe() {
+  it('returns list of categories', function it(done) {
+    nrk.tv.mobil.categories(function categoriesCb(err, data, resp) {
+      assert.ifError(err);
+
+      const schema = Joi.array().items(Joi.object().keys({
+        categoryId: Joi.string().example('dokumentar'),
+        displayValue: Joi.string().example('Dokumentar'),
+        availableFilters: Joi.array().items(Joi.string().valid(
+          [ 'POPULAR', 'RECOMMENDED', 'RECENT' ]
+        )),
+      }));
+
+      Joi.validate(data, schema, done);
+    });
+  });
+});
+
+describe('programs()', function describe() {
+  it('returns list of programs', function it(done) {
+    this.timeout(10000);
+
+    nrk.tv.mobil.programs(null, function seriesCb(err, data, resp) {
+      assert.ifError(err);
+
+      const schema = Joi.array().items(Joi.object().keys({
+        title: Joi.string().example('Diktaturet'),
+        description: Joi.string().allow(''),
+        episodeNumberOrDate: Joi.string().example('7:8'),
+        mediaUrl: Joi.string(),
+        programId: Joi.string().example('mynt14000715'),
+        seriesId: Joi.string().example('diktaturet'),
+        seriesImageId: Joi.string().regex(/^[a-zA-Z0-9_-]+$/),
+        imageId: Joi.string().regex(/^[a-zA-Z0-9_-]+$/),
+        plugImageId: Joi.string().regex(/^[a-zA-Z0-9_-]+$/),
+        source: Joi.string().example('plugs'),
+        category: Joi.object().keys({
+          categoryId: Joi.string().example('underholdning'),
+          displayValue: Joi.string().example('Underholdning'),
+          priority: Joi.number().integer(),
+        }),
+        seasonId: Joi.number().integer(),
+        promoText: Joi.string(),
+        usageRights: Joi.object().keys({
+          availableFrom: Joi.number().integer(),
+          availableTo: Joi.number().integer(),
+          hasNoRights: Joi.boolean(),
+          geoblocked: Joi.boolean(),
+        }),
+        liveProgramMetadata: Joi.object().keys({
+          startTime: Joi.number().integer(),
+          endTime: Joi.number().integer(),
+          nextProgram: Joi.string(),
+        }),
+        plugTitle: Joi.string(),
+        legalAge: Joi.string().valid(['A', '6', '9', '12', '15', '18']),
+        labelText: Joi.string(),
+        channelId: Joi.string(),
+        epgEntries: Joi.array().items(Joi.object()),
+        size: Joi.string().valid('small', 'medium', 'large', 'random'),
+        applicationType: Joi.string().example('bigscreen'),
+        duration: Joi.number().integer(),
+        isAvailable: Joi.boolean(),
+      }));
+
+      Joi.validate(data, schema, done);
+    });
+  });
+
+  it('returns list of programs', function it(done) {
+    this.timeout(10000);
+
+    nrk.tv.mobil.programs('mynt14000715', function seriesCb(err, data, resp) {
+      assert.ifError(err);
+
+      const schema = Joi.object().keys({
+        title: Joi.string().example('Diktaturet'),
+        description: Joi.string().allow(''),
+        episodeNumberOrDate: Joi.string().example('7:8'),
+        mediaUrl: Joi.string(),
+        programId: Joi.string().example('mynt14000715'),
+        seriesId: Joi.string().example('diktaturet'),
+        seriesImageId: Joi.string().regex(/^[a-zA-Z0-9_-]+$/),
+        imageId: Joi.string().regex(/^[a-zA-Z0-9_-]+$/),
+        category: Joi.object().keys({
+          categoryId: Joi.string().example('underholdning'),
+          displayValue: Joi.string().example('Underholdning'),
+          priority: Joi.number().integer(),
+        }),
+        seasonId: Joi.number().integer(),
+        usageRights: Joi.object().keys({
+          availableFrom: Joi.number().integer(),
+          availableTo: Joi.number().integer(),
+          hasNoRights: Joi.boolean(),
+          geoblocked: Joi.boolean(),
+        }),
+        legalAge: Joi.string().valid(['A', '6', '9', '12', '15', '18']),
+        numberOfAvailableEpisodes: Joi.number().integer(),
+        epgEntries: Joi.array(),
+        duration: Joi.number().integer(),
+        isAvailable: Joi.boolean(),
+        fullTitle: Joi.string().example('Diktaturet 7:8'),
+        shortIndexPoints: Joi.array().items(Joi.object().keys({
+          title: Joi.string().example('Velkommen til Diktaturet'),
+          startPoint: Joi.number().integer(),
+          partId: Joi.number().integer(),
+          part: Joi.number().integer(),
+        })),
+        hasSubtitles: Joi.boolean(),
+        hasReview: Joi.boolean(),
+        series: Joi.object().keys({
+          seriesId: Joi.string().example('diktaturet'),
+          title: Joi.string().example('Diktaturet'),
+          description: Joi.string(),
+          seasonIds: Joi.array().items(Joi.object().keys({
+            id: Joi.number().integer(),
+            name: Joi.string().example('Sesong 1'),
+          })),
+          imageId: Joi.string().regex(/^[a-zA-Z0-9_-]+$/),
+          category: Joi.object().keys({
+            categoryId: Joi.string().example('underholdning'),
+            displayValue: Joi.string().example('Underholdning'),
+            priority: Joi.number().integer(),
+          }),
+          programs: Joi.array().items(Joi.object().keys({
+            title: Joi.string().example('Diktaturet'),
+            description: Joi.string(),
+            episodeNumberOrDate: Joi.string().example('7:8'),
+            programId: Joi.string().example('mynt14000715'),
+            seriesId: Joi.string().example('diktaturet'),
+            imageId: Joi.string().regex(/^[a-zA-Z0-9_-]+$/),
+            category: Joi.object().keys({
+              categoryId: Joi.string().example('underholdning'),
+              displayValue: Joi.string().example('Underholdning'),
+              priority: Joi.number().integer(),
+            }),
+            seasonId: Joi.number().integer(),
+            usageRights: Joi.object().keys({
+              availableFrom: Joi.number().integer(),
+              availableTo: Joi.number().integer(),
+              hasNoRights: Joi.boolean(),
+              geoblocked: Joi.boolean(),
+            }),
+            legalAge: Joi.string().valid(['A', '6', '9', '12', '15', '18']),
+            duration: Joi.number().integer(),
+            isAvailable: Joi.boolean(),
+          })),
+        }),
+        contributors: Joi.array(),
+        relativeOriginUrl: Joi.string(),
+        more: Joi.array().label('Related Shows'),
+        statistics: Joi.object(),
+      });
+
+      Joi.validate(data, schema, done);
+    });
+  });
+});
+
+describe('search()', function describe() {
+  it('returns matching list of programs', function it(done) {
+    this.timeout(10000);
+
+    nrk.tv.mobil.search('Side om side', function seriesCb(err, data, resp) {
+      assert.ifError(err);
+
+      const schema = Joi.object().keys({
+        metaData: Joi.object(),
+        hits: Joi.array().items(Joi.object().keys({
+          type: Joi.string().valid('serie', 'episode', 'program'),
+          hit: Joi.object().keys({
+            objectId: Joi.string().example('side-om-side'),
+            title: Joi.string().example('Side om side'),
+            description: Joi.string(),
+            imageId: Joi.string().regex(/^[a-zA-Z0-9_-]+$/),
+            seasons: Joi.any().allow(null),
+            category: Joi.object(),
+            duration: Joi.number().allow(null),
+            expires: Joi.any().allow(null),
+            seriesId: Joi.string().allow(null).example('side-om-side'),
+            seasonNumber: Joi.string().allow(null),
+            seriesTitle: Joi.string().allow(null),
+            episodeNumberOrDate: Joi.string().allow(null).example('7:8'),
+            episodeTitle: Joi.string().allow(null),
+            aldersgrense: Joi.number().integer().allow(null),
+            usageRights: Joi.object().keys({
+              availableFrom: Joi.number().integer(),
+              availableTo: Joi.number().integer(),
+              hasNoRights: Joi.boolean(),
+              geoblocked: Joi.boolean(),
+            }).allow(null),
+            programId: Joi.string().allow(null).example('mynt14000715'),
+          }),
+          highlights: Joi.array().allow(null),
+        })),
+      });
+
+      Joi.validate(data, schema, done);
+    });
+  });
+});
