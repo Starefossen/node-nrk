42b043c592da448ca004483fd0e429a65672e0e3
Hans Kristian Flaatten <hans.kristian.flaatten@dnt.no>
2016-01-09 18:11:00 +0100

    fix(eslint): line exceeding maximum line length of 100

--- index.js
+++ index.js
@@ -20,7 +20,11 @@ module.exports._opts = function _opts(agent) {
 
   return {
     headers: {
-      'user-agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 6_0 like Mac OS X) AppleWebKit/536.26 (KHTML, like Gecko) Version/6.0 Mobile/10A5376e Safari/8536.25',
+      'user-agent': [
+        'Mozilla/5.0 (iPhone; CPU iPhone OS 6_0 like Mac OS X)',
+        'AppleWebKit/536.26 (KHTML, like Gecko) Version/6.0',
+        'Mobile/10A5376e Safari/8536.25',
+      ].join(' '),
       'accept': 'application/json, text/javascript, */*; q=0.01',
       'x-requested-with': 'XMLHttpRequest',
     },
